20532950925879b69f5f6c93b9ca03367ea54509
Jakub Januszewski <janir3g@gmail.com>
2024-06-07 22:45:15 +0200

    Adding oauth

--- backend/src/main/java/com/poznan/put/rest/webservice/restapi/security/SpringSecurityConfiguration.java
+++ backend/src/main/java/com/poznan/put/rest/webservice/restapi/security/SpringSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -16,20 +17,29 @@ import java.util.List;
 @Configuration
 public class SpringSecurityConfiguration {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
-                .cors(Customizer.withDefaults()) // by default use a bean by the name of corsConfigurationSource
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .csrf().disable() // enable it after testing
-                .httpBasic(Customizer.withDefaults())
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/", "/index", "/oauth2/authorization/google").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/oauth2/authorization/google")
+                                .defaultSuccessUrl("/home", true)
+                                .failureUrl("/login?error=true")
+                );
+
+        return http.build();
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
